b247638b718af47b415da55b0622be8f27a6d1e7
hsvsll <wdyj0.0@foxmail.com>
2017-05-08 18:49:24 +0800

    DownLoadManage

--- BreakPointHttp/app/src/main/java/com/example/qiyue/breakpointhttp/DownLoadIntentService.java
+++ BreakPointHttp/app/src/main/java/com/example/qiyue/breakpointhttp/DownLoadIntentService.java
@@ -1,26 +1,13 @@
 package com.example.qiyue.breakpointhttp;
 
-import android.Manifest;
-import android.app.Activity;
 import android.app.DownloadManager;
 import android.app.IntentService;
 import android.content.BroadcastReceiver;
-import android.content.Intent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.database.ContentObserver;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Environment;
 import android.os.Handler;
-import android.support.v4.app.ActivityCompat;
-import android.util.Log;
-import android.webkit.MimeTypeMap;
-import android.widget.Toast;
-
-import java.io.File;
+import android.os.Message;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -30,26 +17,21 @@ import java.io.File;
  * helper methods.
  */
 public class DownLoadIntentService extends IntentService {
-    // TODO: Rename actions, choose action names that describe tasks that this
-    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String TAG = DownLoadIntentService.class.getSimpleName();
     private static final String ACTION_FOO = "com.example.qiyue.breakpointhttp.action.FOO";
     private static final String ACTION_BAZ = "com.example.qiyue.breakpointhttp.action.BAZ";
 
-    // TODO: Rename parameters
     private static final String EXTRA_PARAM1 = "com.example.qiyue.breakpointhttp.extra.PARAM1";
     private static final String EXTRA_PARAM2 = "com.example.qiyue.breakpointhttp.extra.PARAM2";
 
-    private long mTaskId;
 
-    private DownloadManager downloadManager;
-
-    private String mDownloadPath;
-
-    private DownloadChangeObserver downloadObserver;
-
-    private Uri CONTENT_URI;
-
-    private IntentFilter intentFilter;
+    public interface OnProgressListener {
+        /**
+         * 下载进度
+         * @param fraction 已下载/总大小
+         */
+        void onProgress(float fraction);
+    }
 
     public DownLoadIntentService() {
         super("DownLoadIntentService");
@@ -61,7 +43,6 @@ public class DownLoadIntentService extends IntentService {
      *
      * @see IntentService
      */
-    // TODO: Customize helper method
     public static void startSingleThreadDownLoad(Context context, String apkUrl, String downloadPath) {
         Intent intent = new Intent(context, DownLoadIntentService.class);
         intent.setAction(ACTION_FOO);
@@ -70,20 +51,14 @@ public class DownLoadIntentService extends IntentService {
         context.startService(intent);
     }
 
-    /**
-     * Starts this service to perform action Baz with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, DownLoadIntentService.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
+    public Handler downLoadHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    };
+
+
 
     @Override
     protected void onHandleIntent(Intent intent) {
@@ -91,12 +66,8 @@ public class DownLoadIntentService extends IntentService {
             final String action = intent.getAction();
             if (ACTION_FOO.equals(action)) {
                 final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                mDownloadPath = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionFoo(param1);
-            } else if (ACTION_BAZ.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
                 final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionBaz(param1, param2);
+                handleActionFoo(param1);
             }
         }
     }
@@ -107,152 +78,39 @@ public class DownLoadIntentService extends IntentService {
      */
     private void handleActionFoo(String apkUrl) {
 
-        //创建下载任务
-        downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-
-        CONTENT_URI = Uri.parse(apkUrl);
-        downloadObserver = new DownloadChangeObserver(null);
-        getContentResolver().registerContentObserver(CONTENT_URI, true, downloadObserver);
-
-        DownloadManager.Request request = new DownloadManager.Request(CONTENT_URI);
-
-        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
-        request.setAllowedOverRoaming(false);
-
-        /**设置通知栏是否可见*/
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_HIDDEN);
-
-        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
-        String mimeString = mimeTypeMap.getMimeTypeFromExtension(MimeTypeMap.getFileExtensionFromUrl(apkUrl));
-        request.setMimeType(mimeString);
-
-
-        request.setDescription("Some descrition");
-        request.setTitle("Some title");
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            request.allowScanningByMediaScanner();
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        }
-        request.setVisibleInDownloadsUi(true);
-
-        //sdcard的目录下download文件夹,必须设置
-        request.setDestinationInExternalPublicDir(mDownloadPath,"DownLoadApk.apk");
-//        request.setDestinationInExternalFilesDir();
-
-
-        mTaskId = downloadManager.enqueue(request);
-
-        intentFilter = new IntentFilter();
-        intentFilter.addAction(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
-        intentFilter.addAction(DownloadManager.ACTION_NOTIFICATION_CLICKED);
-        registerReceiver(receiver, intentFilter);
-
-
     }
+
+    /**
+     * 注册广播
+     */
+    private void registerBroadcast() {
+        /**注册service 广播 1.任务完成时 2.进行中的任务被点击*/
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    /**
+     * 注销广播
+     */
+    private void unregisterBroadcast() {
+        if (receiver != null) {
+            unregisterReceiver(receiver);
+            receiver = null;
+        }
+    }
+
 
     private BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Log.i("TAG","getAction : "+ intent.getAction());
+
         }
     };
-
-    class DownloadChangeObserver extends ContentObserver {
-
-        public DownloadChangeObserver(Handler handler) {
-            super(handler);
-            // TODO Auto-generated constructor stub
-        }
-
-
-        @Override
-        public void onChange(boolean selfChange) {
-            int[] bytesAndStatus = getBytesAndStatus(mTaskId);
-            int currentSize = bytesAndStatus[0];//当前大小
-            int totalSize = bytesAndStatus[1];//总大小
-            int status = bytesAndStatus[2];//下载状态
-            Toast.makeText(getApplicationContext(),"当前下载文件大小 :"+currentSize,Toast.LENGTH_SHORT).show();
-            Log.i("TAG","当前下载文件大小 : "+ currentSize);
-        }
-
-
-    }
-
-    public int[] getBytesAndStatus(long downloadId) {
-        int[] bytesAndStatus = new int[] { -1, -1, 0 };
-        DownloadManager.Query query = new DownloadManager.Query().setFilterById(downloadId);
-        Cursor c = null;
-        try {
-            c = downloadManager.query(query);
-            if (c != null && c.moveToFirst()) {
-                bytesAndStatus[0] = c.getInt(c.getColumnIndexOrThrow(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR));
-                bytesAndStatus[1] = c.getInt(c.getColumnIndexOrThrow(DownloadManager.COLUMN_TOTAL_SIZE_BYTES));
-                bytesAndStatus[2] = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS));
-            }
-        } finally {
-            if (c != null) {
-                c.close();
-            }
-        }
-        return bytesAndStatus;
-    }
-
-    private void checkDownLoadStatus() {
-        DownloadManager.Query query = new DownloadManager.Query();
-        query.setFilterById(mTaskId);
-        Cursor c = downloadManager.query(query);
-        if(c.moveToFirst()){
-            int status = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS));
-            switch (status){
-                case DownloadManager.STATUS_PAUSED:
-                    break;
-                case DownloadManager.STATUS_PENDING:
-                    break;
-                case DownloadManager.STATUS_RUNNING:
-                    int currFileSize = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR));
-                    Log.i("TAG","当前下载文件大小 : "+ currFileSize);
-                    break;
-                case DownloadManager.STATUS_SUCCESSFUL:
-                    Log.i("TAG","文件下载完成 ");
-                    installAPK(new File(mDownloadPath));
-                    break;
-                case DownloadManager.STATUS_FAILED:
-                    break;
-
-            }
-        }
-    }
-
-    //下载到本地后执行安装
-    protected void installAPK(File file) {
-        if (!file.exists()) return;
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        Uri uri = Uri.parse("file://" + file.toString());
-        intent.setDataAndType(uri, "application/vnd.android.package-archive");
-        //在服务中开启activity必须设置flag,后面解释
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-    }
-
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if(receiver != null){
-            unregisterReceiver(receiver);
-        }
-
-        if (downloadObserver != null) {
-            getContentResolver().unregisterContentObserver(downloadObserver);
-        }
     }
+
 }
--- BreakPointHttp/app/src/main/java/com/example/qiyue/breakpointhttp/MainActivity.java
+++ BreakPointHttp/app/src/main/java/com/example/qiyue/breakpointhttp/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.qiyue.breakpointhttp;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.DownloadManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.IBinder;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -27,8 +30,53 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.pbDownLoad)
     ProgressBar mProgressBar;
 
+    @BindView(R.id.tvProgressText)
+    TextView mProgressText;
+
     private static final String APK_URL = "http://i.ahggwl.com/hytpay/hyt.apk";
-    private static final String DOWN_LOAD_PATH = "/Users/qiyue/Downloads/apk";
+    private static final String DOWN_LOAD_PATH = Environment.DIRECTORY_DOWNLOADS;
+
+    private String TAG = this.getClass().getSimpleName();
+
+    private boolean isBindService;
+
+    private int mProgressSize;
+
+    private ServiceConnection conn = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            DownloadService.DownloadBinder binder = (DownloadService.DownloadBinder) service;
+            DownloadService downloadService = binder.getService();
+
+            //接口回调，下载进度
+            downloadService.setOnProgressListener(new DownloadService.OnProgressListener() {
+                @Override
+                public void onProgress(float fraction) {
+                    mProgressSize = (int) (fraction * 100);
+                    mProgressText.setText(mProgressSize+"%");
+                    mProgressBar.setProgress(mProgressSize);
+
+                    //判断是否真的下载完成进行安装了，以及是否注册绑定过服务
+                    if (fraction == DownloadService.UNBIND_SERVICE && isBindService) {
+                        unbindService(conn);
+                        isBindService = false;
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+    };
+
+    public void bindService() {
+        Intent intent = new Intent(this, DownloadService.class);
+        intent.putExtra(DownloadService.BUNDLE_KEY_DOWNLOAD_URL, APK_URL);
+        intent.putExtra(DownloadService.BUNDLE_KEY_DOWNLOAD_PATH, DOWN_LOAD_PATH);
+        isBindService = bindService(intent, conn, BIND_AUTO_CREATE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +94,8 @@ public class MainActivity extends AppCompatActivity {
         switch (v.getId()){
             case R.id.tvSingleThreadDownload:
                 verifyStoragePermissions(MainActivity.this);
-//                DownLoadIntentService.startSingleThreadDownLoad(this,APK_URL,DOWN_LOAD_PATH);
                 break;
             case R.id.tvMoreThreadDownload:
-                Log.i("TAG","TEST ----------");
                 break;
             case R.id.tvBreakPointHttp:
                 break;
@@ -67,18 +113,16 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
     public void verifyStoragePermissions(Activity activity) {
-        // Check if we have write permission
         int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
         if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
             ActivityCompat.requestPermissions(
                     activity,
                     PERMISSIONS_STORAGE,
                     REQUEST_EXTERNAL_STORAGE
             );
         }else {
-            DownLoadIntentService.startSingleThreadDownLoad(MainActivity.this,APK_URL,DOWN_LOAD_PATH);
+            bindService();
         }
     }
 
@@ -91,21 +135,26 @@ public class MainActivity extends AppCompatActivity {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                    DownLoadIntentService.startSingleThreadDownLoad(this,APK_URL,DOWN_LOAD_PATH);
+                    bindService();
                 } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
                 }
                 return;
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
+    }
+
+    /**
+     * 删除上次更新存储在本地的apk
+     */
+    private void removeOldApk() {
+        //获取老ＡＰＫ的存储路径
+//        File fileName = new File(SPUtil.getString(Constant.SP_DOWNLOAD_PATH, ""));
+//        LogUtil.i(TAG, "老APK的存储路径 =" + SPUtil.getString(Constant.SP_DOWNLOAD_PATH, ""));
+//
+//        if (fileName != null && fileName.exists() && fileName.isFile()) {
+//            fileName.delete();
+//            LogUtil.i(TAG, "存储器内存在老APK，进行删除操作");
+//        }
     }
 
 }
